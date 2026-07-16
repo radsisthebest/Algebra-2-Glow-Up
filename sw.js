17b3433d86138df0c15da213a90179f4a2316fa9
radhika <radhikatchandra@outlook.com>
2026-07-15 17:06:03 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "honors-algebra-2-prep-v54";
+const CACHE = "honors-algebra-2-prep-v55";
 const APP_FILES = ["./", "./index.html", "./login.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
